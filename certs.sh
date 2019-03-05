c598d0fdb7cc41bd2d2e9765f31bf6f980bf7bc1
Tom <tdessi@tiscali.com>
2019-03-05 21:59:40 +0100

    exclude README file in dir /etc/letsencrypt/live/

--- certs.sh
+++ certs.sh
@@ -7,7 +7,7 @@ if [ -n "$CERTS" ]; then
         || exit 1
 
     mkdir -p /etc/haproxy/certs
-    for site in `ls -1 /etc/letsencrypt/live`; do
+    for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
         cat /etc/letsencrypt/live/$site/privkey.pem \
           /etc/letsencrypt/live/$site/fullchain.pem \
           | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
