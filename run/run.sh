2fe7b85c052b928d7d5d69e9724f9c338fb33c86
Tom <tdessi@tiscali.com>
2019-03-07 22:30:30 +0100

    run environment (run cmd + docker-compose.yml)

---
+++ run/run.sh
@@ -0,0 +1,8 @@
+docker run --name lb -d \
+    -e CERTS=my.domain \
+    -e EMAIL=my.mail \
+    -v $PWD/data/letsencrypt:/etc/letsencrypt \
+    -v $PWD/data/haproxy.cfg:/etc/haproxy/haproxy.cfg \
+    --network web_network \
+    -p 80:80 -p 443:443 \
+    tomdess/docker-haproxy-letsencrypt.debian9:latest
