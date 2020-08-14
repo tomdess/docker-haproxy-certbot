150a1365133b81f61de5c4d6620fec5881eb1f09
Tom <tom@tiscali.it>
2020-08-14 16:05:31 +0200

    fix missing cron environment with /container.env file creation at container bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# save container environment variables to use it
+# in cron scripts
+
+declare -p | grep -Ev '^declare -[[:alpha:]]*r' > /container.env
+
+# go!
 /certs.sh && supervisord -c /etc/supervisord.conf -n
